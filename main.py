20489cf1eb3b6f6de38187d15182b612ac120265
Towsif <ali.towsif1@gmail.com>
2023-04-04 00:46:24 +0800

    Initiated DB

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Depends
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind = engine)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
