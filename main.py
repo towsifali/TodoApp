1d50a3cf38607c1135a419cfb675500f27b824ab
Towsif <ali.towsif1@gmail.com>
2023-04-04 11:03:51 +0800

    Initial CRUD methods

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Depends
+from typing import Optional
+from fastapi import FastAPI, Depends,HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -14,6 +16,73 @@ def get_db():
     finally:
         db.close()
 
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0,lt=6)
+    complete: bool
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int,db: Session = Depends(get_db)):
+    res = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    
+    if res is not None:
+        return res
+    raise http_exception()
+
+@app.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+    
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status' : 201,
+        'transaction' : 'Successful'
+    }
+
+@app.put("/{todo_id}")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    
+    if todo_model is None:
+        raise http_exception()
+    
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+    
+    db.add(todo_model)
+    db.commit()
+    
+    return successful_response()
+
+@app.delete("/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is None:
+        raise http_exception()
+    
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    db.commit()
+    
+    return successful_response()
+
+def http_exception():
+    return HTTPException(status_code = 404,detail = "Todo not found")
+
+def successful_response():
+    return {
+        'status' : 200,
+        'transaction' : 'Successful'
+    }
