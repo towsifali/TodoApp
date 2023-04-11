e9311ce0e581606deae6f6690e4d68cdeb64cf58
Towsif <ali.towsif1@gmail.com>
2023-04-11 16:38:16 +0800

    Authenticate Users (JWT)

--- auth.py
+++ auth.py
@@ -9,7 +9,8 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from jose import jwt
 
-
+SECRET_KEY = "l0mRWur8ICG7Y8pRezA650Vgfih1BlfY"
+ALGORITHM = "HS256"
 
 class CreateUser(BaseModel):
     username: str
@@ -21,6 +22,8 @@ class CreateUser(BaseModel):
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -45,11 +48,23 @@ def authenticate_user(username: str, password: str, db):
         return False
     return user
 
+def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
+    encode = {"sub":username,"id":user_id}
+    
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    encode.update({"exp":expire})
+    
+    return jwt.encode(encode,SECRET_KEY,algorithm=ALGORITHM)
+
 @app.post("/create/user")
 async def create_new_user(create_user: CreateUser,db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
-    create_user_model.username = create_user.email
+    create_user_model.username = create_user.username
     create_user_model.first_name = create_user.first_name
     create_user_model.last_name = create_user.last_name
     
@@ -68,4 +83,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     user = authenticate_user(form_data.username,form_data.password,db)
     if not user:
         raise HTTPException(status_code=404,detail="User not found.")
-    return "User Validated"
+    
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(user.username,user.id,expires_delta=token_expires)
+    
+    return {"token":token}
