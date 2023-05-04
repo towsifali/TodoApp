fffbd736bfd2a4ac33b893dacaefc8b808adeed4
Towsif <ali.towsif1@gmail.com>
2023-05-04 14:34:11 +0800

    Layout html to reduce code duplication

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("..")
 
-from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi import Depends, HTTPException, status, APIRouter, Request
 from pydantic import BaseModel
 from typing import Optional
 import models
@@ -11,6 +11,8 @@ from database import SessionLocal, engine
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
@@ -28,6 +30,8 @@ class CreateUser(BaseModel):
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -120,6 +124,14 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
                                 expires_delta=token_expires)
     return {"token": token}
 
+
+@router.get("/",response_class=HTMLResponse)
+async def authentication_page(request: Request):
+    return templates.TemplateResponse("login.html",{"request":request})
+
+@router.get("/register/",response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html",{"request":request})
 
 #Exceptions
 def get_user_exception():
